137d26ff232f20c982c4bcfee55a0f9699527927
ard1107 <96828597+ard1107@users.noreply.github.com>
2023-06-14 20:44:51 +0530

    Add files via upload

---
+++ log/mainapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required
+def dashboard(request):  #mainScreen
+    return render(request,'registration/dashboard.html',{'section':'dashboard'})
